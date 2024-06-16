e6beb27376dc0049e679a685a5e50b6d5fd9280b
Dustin Rush <dustin@ionflow.com>
2024-06-16 15:12:04 -0700

    removing extras

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 from . import nodes
 
 NODE_CLASS_MAPPINGS = {
+    "ETN_SendImageWebSocket": nodes.SendImageWebSocket,
     "ETN_SendImageMQTT": nodes.SendImageMQTT,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ETN_SendImageWebSocket": "Send Image (MQTT)",
+    "ETN_SendImageWebSocket": "Send Image (WebSocket)",
+    "ETN_SendImageMQTT": "Send Image (MQTT)",
 }
--- nodes.py
+++ nodes.py
@@ -4,6 +4,36 @@ import numpy as np
 from server import PromptServer, BinaryEventTypes
 
 
+class SendImageWebSocket:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"images": ("IMAGE",)}}
+
+    RETURN_TYPES = ()
+    FUNCTION = "send_images"
+    OUTPUT_NODE = True
+    CATEGORY = "external_tooling"
+
+    def send_images(self, images):
+        results = []
+        for tensor in images:
+            array = 255.0 * tensor.cpu().numpy()
+            image = Image.fromarray(np.clip(array, 0, 255).astype(np.uint8))
+
+            server = PromptServer.instance
+            server.send_sync(
+                BinaryEventTypes.UNENCODED_PREVIEW_IMAGE,
+                ["PNG", image, None],
+                server.client_id,
+            )
+            results.append(
+                # Could put some kind of ID here, but for now just match them by index
+                {"source": "websocket", "content-type": "image/png", "type": "output"}
+            )
+
+        return {"ui": {"images": results}}
+
+
 class SendImageMQTT:
     @classmethod
     def INPUT_TYPES(s):
